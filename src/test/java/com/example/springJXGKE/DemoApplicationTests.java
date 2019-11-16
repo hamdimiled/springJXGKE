ea9f061413e1f5c57ffc5f1c37d9857545003436
hamdimiled <hamdismiled@gmail.com>
2019-11-16 16:02:04 +0100

    Initial import

---
+++ src/test/java/com/example/springJXGKE/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springJXGKE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
